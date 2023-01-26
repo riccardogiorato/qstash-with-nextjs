d0395fd0b8be994b138e21740c22e7db6f6c7f58
Riccardo Giorato <riccardo@teero.com>
2023-01-26 15:29:34 +0100

    randomly fail the request

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -36,6 +36,11 @@ export default async function handler(
   // await 5 seconds
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
+  // randomly fail the request
+  if (Math.random() < 0.5) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+
   console.log(`Newsletter active for ${email}`);
 
   res.status(200).json({ message: `Newsletter active for ${email}` });
